2d0385975491e71a94ee2fbee8f1dbdf5db05f1a
Mariano Bello <33380377+jbtis@users.noreply.github.com>
2020-05-24 17:35:24 -0500

    Update gh-pages.js

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require("gh-pages")
 ghpages.publish(
   "__sapper__/export/missions.pe",
   {
-    branch: "gh-pages",
+    branch: "master",
     repo: "https://github.com/jbtis/missions.pe.git",
     user: {
       name: "Mariano Bello",
